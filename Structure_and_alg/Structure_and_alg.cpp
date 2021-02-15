8d214c563fe6d348830d0a729b2df371496fc6ef
vbg911 <vbg911@gmail.com>
2021-02-15 12:47:00 +0300

    реализовал два метода удаления.

--- Structure_and_alg/Structure_and_alg.cpp
+++ Structure_and_alg/Structure_and_alg.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
 void delFirstMetod(int* x, int n, int key);
+void delOtherMetod(int* x, int n, int key);
+void copy(int* copy, int copy_len, int final_len);
+void print_arr(int* mass, int len, string name = "");
 
 void main()
 {
@@ -14,21 +17,14 @@ void main()
 		cout << "arr[" << i << "]=";
 		cin >> arr[i];
 	}
-	for (int i=0;i<mass;i++)
-	{
-		cout << "arr[" << i << "]=" << arr[i]<<endl;
-	}
+	print_arr(arr,mass,"Начальный массив");
 	delFirstMetod(arr,mass,2);
-	for (int i = 0; i < mass; i++)
-	{
-		cout << "arr[" << i << "]=" << arr[i] << endl;
-	}
+	print_arr(arr,mass,"Массив после преобразования");
 	system("pause");
 }
 
-void delFirstMetod(int* x,int n, int key)
-{
-	cout << x << n << key << endl;
+void delFirstMetod(int* x,int n, int key) {
+	int c_len = n;
 	int i = 1;
 	while (i<=n)
 	{
@@ -45,5 +41,39 @@ void delFirstMetod(int* x,int n, int key)
 			i++;
 		}
 	}
+	copy(x, c_len, n);
 }
 
+void delOtherMetod(int* x, int n, int key) {
+	int c_len = n;
+	int j = 1;
+	for (int i = 1; i < n; i++) {
+		x[j] = x[i];
+		if (x[i] != key) {
+			j++;
+		}
+	}
+	n = j;
+	copy(x, c_len, n);
+}
+
+void print_arr(int* mass,int len,string name ) { // вывод массива на экран
+	cout << "------Ввывод массива (начало) "<<name<<" ------" << endl;
+	for (int i = 0; i < len; i++) {
+		cout << "mass["<<i<<"]=" << mass[i] << endl;
+	}
+	cout << "------Ввывод массива (конец) "<<name<<" ------" << endl;
+}
+
+void copy(int* tocopy,int copy_len, int final_len) { // copy_len - длина массива до копирования final_len - длина массива после копирования
+	int* temparr = new int[copy_len];// массив куда мы копируем исходный
+	for (int i = 0; i < copy_len; i++) {
+		temparr[i] = tocopy[i];
+	}
+	int* copy = new int[final_len];
+	for (int i = 0; i < final_len; i++) {
+		copy[i] = temparr[i];
+	}
+	delete[]temparr;
+	print_arr(copy, final_len,"Функция copy");
+}
